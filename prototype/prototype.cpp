2cbab1db20733fec46ada271430efb4fa28b5d7a
Paris Hom <phom001@ucr.edu>
2019-10-28 19:10:16 -0700

    prototype for waitpid(), execvp(), fork()

---
+++ prototype/prototype.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sys/wait.h>
+
+using namespace std;
+
+void childProcess();
+void parentProcess();
+
+// for testing fork(),waitpid(),execvp()
+int main(int argc, char** argv){
+    // argv end with NULL
+   //char* args[] = {"/bin/ls", "-l", "/u/userid/dirname", NULL};
+    pid_t pid;
+    int status;
+
+    // create a new process that runs the same instructions after fork() instruction
+    pid = fork();
+    if (pid == -1){
+      perror("fork");
+      exit(1);
+      }
+    if (pid == 0){ //fork() returning 0 means child process
+        childProcess();
+        
+        // will execute ls command
+        //execvp takes in name of programfile, and pointer to array of char[] char**
+        execvp("ls", argv);
+        
+        //if reaches here, execvp failed
+        perror("execvp");
+
+    } else { //parent process
+          //waitpid suspends execution of calling process until a child(pid) has changed state(status)
+         pid_t w = waitpid(pid,&status, 0);
+          //wait for any child process to finish first before resuming parent process
+         if (w < 0){
+             printf("wait");
+         }
+        parentProcess();
+        execvp("echo \"hello\"", argv);
+        cout << "Parent process finished" << endl;
+    }
+    return 0;
+}
+
+void childProcess(){
+    int counter = 0;
+    for (int i = 0; i < 3; i++){
+        counter++;
+        cout << "child process occuring.. counter: " << counter << endl;
+    }
+}
+
+void parentProcess(){
+    int counter = 0;
+    for (int i = 0; i < 3; i++){
+        counter++;
+         cout << "parent process occuring.. counter: " << counter << endl;
+    }
+}
